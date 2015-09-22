09747b3687b984fea5a33bf1d2cb9ef3913d537e
Vallium <anton.alliot@gmail.com>
2015-09-22 16:31:22 +0200

    Camera class

---
+++ includes/Camera.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Camera.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/09/22 16:03:20 by aalliot           #+#    #+#             */
+/*   Updated: 2015/09/22 16:03:22 by aalliot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAMERA_HPP
+# define CAMERA_HPP
+
+# define GLM_FORCE_RADIANS
+# include <glm/glm.hpp>
+# include <string>
+# include <vector>
+# include <SOIL/SOIL.h>
+
+# include <GL/glew.h>
+
+# define CAMERA_SPEED 20
+
+enum Direction {UP, DOWN, FORWARD, BACKWARD, LEFT, RIGHT, BOOST_PLUS, BOOST_MOINS};
+
+class Camera {
+public:
+	glm::vec3	_pos;
+	glm::vec3	_front;
+	glm::vec3	_up;
+
+	GLfloat		_yaw;
+	GLfloat		_pitch;
+	GLfloat		_zoom;
+
+	GLfloat		_speed;
+
+	Camera(glm::vec3 p);
+
+	void	ProcessKeyboard(enum Direction dir, GLfloat dt);
+	void	ProcessMouseMovement(GLfloat xoffset, GLfloat yoffset);
+	glm::mat4	GetViewMatrix();
+};
+
+#endif
