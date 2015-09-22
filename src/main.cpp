803cb9e1a5907f691ee639dadbbf777c6ae22336
Anton ALLIOT <aalliot@e3r1p5.42.fr>
2015-09-22 12:09:45 +0200

    ca compile

--- src/main.cpp
+++ src/main.cpp
@@ -39,8 +39,39 @@ std::vector<int>		parse(std::string name) {
 	return out;
 }
 
+int			**create_tab(std::vector<int> pts) {
+	int		**tab = nullptr;
+	int xmin, xmax, ymin, ymax = -1;
+
+	for (int i = 0; i < pts.size(); i += 3) {
+		if (xmin == -1 or pts[i] < xmin)
+			xmin = pts[i];
+		if (xmax == -1 or pts[i] > xmax)
+			xmax = pts[i];
+		if (ymin == -1 or pts[i + 1] < ymin)
+			ymin = pts[i + 1];
+		if (ymax == -1 or pts[i + 1] > ymax)
+			ymax = pts[i + 1];
+	}
+
+	// int		tab[xmax - xmin + 1][ymax - ymin + 1];
+	*tab = new int[xmax - xmin + 1];
+	for (size_t x = 0; x < xmax - xmin; x++) {
+		tab[x] = new int[ymax - ymin + 1];
+		for (size_t y = 0; y < ymax - ymin; y++) {
+			tab[x][y] = -1;
+		}
+	}
+
+	for (int i = 0; i < pts.size(); i += 3) {
+		tab[pts[i]][pts[i + 1]] = pts[i + 2];
+	}
+	// std::cout << xmin << " " << xmax << " " << ymin << " " << ymax << std::endl;
+	return tab;
+}
+
 int			main(int ac, char *av[]) {
-	GLFWwindow* window;
+	GLFWwindow	*window;
 
 	/* Initialize the library */
 	// if (!glfwInit())
@@ -52,19 +83,10 @@ int			main(int ac, char *av[]) {
 	}
 	else {
 		std::vector<int> pts = parse(std::string(av[1]));
-		int xmin, xmax, ymin, ymax = -1;
-		for (int i = 0; i < pts.size() / 3; i++)
-		{
-			if (xmin == -1 or pts[i*3] < xmin)
-				xmin = pts[i*3];
-			if (xmax == -1 or pts[i*3] > xmax)
-				xmax = pts[i*3];
-			if (ymin == -1 or pts[i*3+1] < ymin)
-				ymin = pts[i*3+1];
-			if (ymax == -1 or pts[i*3+1] > ymax)
-				ymax = pts[i*3+1];
-		}
+		int		**tab = create_tab(pts);
+
 	}
+
 	// /* Create a windowed mode window and its OpenGL context */
 	// window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	// if (!window)
